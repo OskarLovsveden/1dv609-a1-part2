b486887b47d3fec0bc15a161c3dcb46aa171ed30
OskarLovsveden <ol222hf@student.lnu.se>
2020-11-05 14:39:25 +0100

    RED

--- test/LyricsAppTests/LyricTests.cs
+++ test/LyricsAppTests/LyricTests.cs
@@ -10,6 +10,7 @@ namespace LyricsAppTests
         [InlineData("We're no strangers to love. You know the rules and so do I", 13)]
         [InlineData("I just wanna tell you how I'm feeling. Gotta make you understand", 12)]
         [InlineData("and both shall row - my love and I", 8)]
+        [InlineData("and both shall row - my love & I", 7)]
         public void ShouldReturnAmountOfWordsInLyricText(string lyricText, int wordAmount)
         {
             Lyric sut = new Lyric(lyricText);
