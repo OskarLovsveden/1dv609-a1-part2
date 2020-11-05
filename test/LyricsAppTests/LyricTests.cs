bef9c36f7897c06cd5bdb58460dbd990cf0c78d0
OskarLovsveden <ol222hf@student.lnu.se>
2020-11-05 13:36:41 +0100

    REFACTOR

--- test/LyricsAppTests/LyricTests.cs
+++ test/LyricsAppTests/LyricTests.cs
@@ -5,13 +5,15 @@ namespace LyricsAppTests
 {
     public class LyricTests
     {
-        [Fact]
-        public void ShouldReturnAmountOfWordsInLyricText()
+        [Theory]
+        [InlineData("Never gonna give you up.", 5)]
+        [InlineData("We're no strangers to love. You know the rules and so do I", 13)]
+        public void ShouldReturnAmountOfWordsInLyricText(string lyricText, int wordAmount)
         {
-            Lyric sut = new Lyric("Never gonna give you up.");
+            Lyric sut = new Lyric(lyricText);
 
             int actual = sut.CountAllWords();
-            int expected = 5;
+            int expected = wordAmount;
 
             Assert.Equal(actual, expected);
         }
