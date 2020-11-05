3f180e135923ce84cb032b5739064f1dc4114933
Viktor Ödman <vo222dq@student.lnu.se>
2020-11-05 11:24:19 +0100

    Updating Folder Structure

--- src/LyricsApp/Program.cs
+++ src/LyricsApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace WeatherApp
+namespace LyricsApp
 {
     class Program
     {
--- test/LyricsAppTests/UnitTest1.cs
+++ test/LyricsAppTests/UnitTest1.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 
-namespace WeatherAppTests
+namespace LyricsAppTests
 {
     public class UnitTest1
     {
