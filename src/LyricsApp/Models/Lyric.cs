f9e9bb18990bf21f7b2f03369ed604ec700484fd
Viktor Ödman <vo222dq@student.lnu.se>
2020-11-12 15:33:55 +0100

    Adding private method StripApostrophes

--- src/LyricsApp/Models/Lyric.cs
+++ src/LyricsApp/Models/Lyric.cs
@@ -5,32 +5,47 @@ namespace Model
     public class Lyric
     {
         private string _lyricText;
+
+        private string _wordPattern = @"(?!'.*')\b[\w'-]+\b";
+        public string LyricText
+        {
+            get => _lyricText;
+
+            set 
+            {
+                Regex r = new Regex(@"\w");
+                if (!r.IsMatch(Regex.Escape(value)))
+                {
+                    throw new ArgumentException("Lyric text cant be empty");
+                }
+                _lyricText = value;
+            }
+        }
         public Lyric(string lyricText)
         {
-            if (lyricText.Length < 1)
-            {
-                throw new ArgumentException("Lyric text cant be empty");
-            }
-            _lyricText = lyricText;
+            LyricText = lyricText;
         }
 
         public int CountAllWords()
         {
-            Regex r = new Regex(@"(?!'.*')\b[\w'-]+\b");
-            return r.Matches(_lyricText).Count;
+            return Regex.Matches(_lyricText, _wordPattern).Count;
         }
 
         public int WordFrequency(string toBeCounted)
         {
-            Regex r = new Regex(@"(?!'.*')\b[\w'-]+\b");
-            if (!r.IsMatch(toBeCounted))
+            if (!Regex.IsMatch(toBeCounted, _wordPattern))
             {
                 throw new ArgumentException();
             }
 
-            string toBeCountedStripped = toBeCounted.Replace("'", "");
-            string lyricsTextStripped = _lyricText.Replace("'", "");
+            string toBeCountedStripped = StripApostrophes(toBeCounted);
+            string lyricsTextStripped = StripApostrophes(_lyricText);
             return Regex.Matches(lyricsTextStripped, "\\b" + Regex.Escape(toBeCountedStripped) + "\\b", RegexOptions.IgnoreCase).Count;
+        }
+
+        private string StripApostrophes(string toBeStripped)
+        {
+            return toBeStripped.Replace("'", "");
         }
     }
 }
