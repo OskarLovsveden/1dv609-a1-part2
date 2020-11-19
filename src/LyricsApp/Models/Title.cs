b3c9004b2af8f2e08b6b1a30fd1f3b08b3d0bb0a
Viktor Ödman <vo222dq@student.lnu.se>
2020-11-19 14:14:16 +0100

    REFACTOR

--- src/LyricsApp/Models/Title.cs
+++ src/LyricsApp/Models/Title.cs
@@ -5,15 +5,17 @@ namespace Model
     public class Title : ITitle
     {
         private string titleName;
-        public Title(string titleName)
-        {
-            TitleName = titleName;
-        }
 
         public string TitleName
         {
             get => titleName;
             private set => titleName = value;
         }
+
+        public Title(string titleName)
+        {
+            TitleName = titleName;
+        }
+
     }
 }
