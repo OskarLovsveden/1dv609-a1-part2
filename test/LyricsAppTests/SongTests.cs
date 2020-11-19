7e1f357af19b7f5057fb0ad4ac4fa69370358072
ol222hf <ol222hf@student.lnu.se>
2020-11-19 15:21:51 +0100

    RED

--- test/LyricsAppTests/SongTests.cs
+++ test/LyricsAppTests/SongTests.cs
@@ -42,6 +42,24 @@ namespace LyricsAppTests
             Assert.Equal(expected, actual);
         }
 
+        [Fact]
+        public void GetSongLyrics_ShouldReturnSongLyrics()
+        {
+            string input = new string('*', 100);
+            Mock<IArtist> mockArtist = GetNewMockArtist();
+            Mock<ITitle> mockTitle = GetNewMockTitle();
+            Mock<ILyric> mockLyric = GetNewMockLyric();
+
+            mockLyric.Setup(lyric => lyric.LyricText).Returns(input);
+
+            Song sut = new Song(mockArtist.Object, mockTitle.Object, mockLyric.Object);
+
+            string expected = input;
+            string actual = sut.getSongLyrics();
+
+            Assert.Equal(expected, actual);
+        }
+
         private Mock<IArtist> GetNewMockArtist() => new Mock<IArtist>();
         private Mock<ITitle> GetNewMockTitle() => new Mock<ITitle>();
         private Mock<ILyric> GetNewMockLyric() => new Mock<ILyric>();
